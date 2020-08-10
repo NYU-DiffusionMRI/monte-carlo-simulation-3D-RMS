14729db37dd5f48e26bbb838f8d269e53a8ecb8a
Hong Hsi Lee <r01222077@ntu.edu.tw>
2020-08-09 23:21:46 -0400

    Update Part 1 Demo 3

---
+++ analysis/setupRMS.m
@@ -0,0 +1,68 @@
+classdef setupRMS < handle
+    
+    properties (GetAccess = public, SetAccess = public)
+        
+    end
+    
+    properties (GetAccess = private, SetAccess = private)
+        
+    end
+    
+    methods (Access = public)
+        function this = setupRMS()
+            
+        end
+        
+        function inputRMS(this,target,fiber,X)
+            fid = fopen(fullfile(target,'fiber.txt'),'w');
+            for i = 1:size(fiber,1)
+                for j = 1:size(fiber,2)
+                    fprintf(fid,sprintf('%u ',fiber(i,j,:)));
+                end
+            end
+            fclose(fid);
+
+            % b table
+            bval = X.bval; bvec = X.bvec;
+            fid = fopen(fullfile(target,'bval.txt'),'w');
+            fprintf(fid,sprintf('%.8f\n',bval));
+            fclose(fid);
+
+            fid = fopen(fullfile(target,'bvec.txt'),'w');
+            for i = 1:size(bvec,1)
+                fprintf(fid,sprintf('%.8f\n',bvec(i,:)));
+            end
+            fclose(fid);
+
+            % Simulation parameters
+            dt = X.time_step; TN = X.step_num; NPar = X.particle_num;
+            Nbvec = size(bvec,1);
+            Ncomp = X.comp_num;
+            voxel_size = X.voxel_size;
+            [NPix1,NPix2,NPix3] = size(fiber);
+            fid = fopen(fullfile(target,'simParamInput.txt'),'w');
+            fprintf(fid,sprintf('%e\n',dt));
+            fprintf(fid,sprintf('%u\n',TN));
+            fprintf(fid,sprintf('%u\n',NPar));
+            fprintf(fid,sprintf('%u\n',Nbvec));
+            fprintf(fid,sprintf('%u\n',Ncomp));
+            fprintf(fid,sprintf('%e\n',voxel_size));
+            fprintf(fid,sprintf('%u\n',NPix1));
+            fprintf(fid,sprintf('%u\n',NPix2));
+            fprintf(fid,sprintf('%u\n',NPix3));
+            fclose(fid);
+
+            D = X.diffusivity;
+            fid = fopen(fullfile(target,'diffusivity.txt'),'w');
+            fprintf(fid,sprintf('%.8f\n',D));
+            fclose(fid);
+
+            T2 = X.T2_relaxation;
+            fid = fopen(fullfile(target,'T2.txt'),'w');
+            fprintf(fid,sprintf('%.8f\n',T2));
+            fclose(fid);
+        end
+    end
+    
+        
+end
---
+++ part1_demo3_simulation.m
@@ -0,0 +1,93 @@
+% Part 1: Diffusivity time-dependence along realistic white matter axons
+% Demo 3: Simulations (Lee, et al., Commun Biol 2020)
+%
+% The purpose of this demontration includes:
+% 1. Performing simulations of diffusion in four geometries in Part 1 Demo
+% 3 (Fig. 2b-f).
+%
+% Reference: 
+% Lee, et al., Communications Biology 2020 (doi:10.1038/s42003-020-1050-x)
+% Lee, et al., Brain Structure and Function 2019 
+%    (doi:10.1007/s00429-019-01844-6)
+%
+% Author: Hong-Hsi Lee (0000-0002-3663-6559)
+
+% Setup directory to this code
+root = pwd;
+
+% Load RMS setup tool
+addpath(fullfile(root,'analysis'));
+
+%% Create input files of RMS
+
+target = cell(4,1);
+target{1} = fullfile(root,'hpc_code','input','I_realistic_axon_mitochondria');
+target{2} = fullfile(root,'hpc_code','input','II_realistic_axon');
+target{3} = fullfile(root,'hpc_code','input','III_caliber_variation');
+target{4} = fullfile(root,'hpc_code','input','IV_undulation');
+
+file = struct([]);
+for i = 1:4
+    filei = load(fullfile(target{i},'fiber.mat'));
+    file(i,1).fiber = filei.fiber;
+end
+
+% Load b-table
+btable = load(fullfile(root,'hpc_code','btable','btable_5shell_90ndir_b1000.mat'));
+bval = btable.bval;
+bvec = btable.bvec;
+
+% Setup simulation parameters and microgeometry
+% The CUDA code only supports at most 3 compartments by default. To
+% increase the number of compartments > 3, please change the Nc_max in line
+% 37 of ./hpc_code/lib/main.cu
+
+% The microgeometry has 3 compartments, labeled as 0 (null compartment), 
+% 1 (cytoplasm), and 2 (mitochondria) in this example.
+% No random walker will be initialized and diffuse in the null compartment.
+% The present code supports exchange with the premeation probability from
+% compartment a to b: P_{a to b} = min(1,sqrt(D_b/D_a));
+sr = setupRMS();
+for i = 1:4
+    clear X
+    X.time_step = 2e-4;                 % Time of each step (ms)
+    X.step_num = 1e3;                   % # step
+    X.particle_num = 1e3;               % # particle
+    X.comp_num = 3;                     % # compartments
+    X.voxel_size = file(1).fiber(1).voxel_size;     % Voxel size (micron)
+    if i ==1
+        % The null compartment must have a diffusivity = 0.
+        % The T2 relaxation must be a positive number for all compartments
+        X.diffusivity = [0 2 2/13];     % Diffusivity in each compartment (micron2/ms)
+        X.T2_relaxation = [80 80 20];   % T2 relaxation time in each compartment (ms)
+    else
+        X.diffusivity = [0 2 2];        % Diffusivity in each compartment (micron2/ms)
+        X.T2_relaxation = [80 80 80];   % T2 relaxation time in each compartment (ms)
+    end
+    X.bval = bval;                      % b-value (ms/micron2) [Nbvec x 1]
+    X.bvec = bvec;                      % gradient directions  [Nbvec x 3]
+    for j = 1:numel(file(i).fiber)
+        % Setup directory to save the input txt files
+        targetj = fullfile(target{i},sprintf('setup%03u',j));
+        mkdir(targetj);
+        % The microgeometry has 3 compartments, labeled as 0 (null 
+        % compartment), 1 (cytoplasm), and 2 (mitochondria) in this example
+        fiberj = file(i).fiber(j).shape;
+        sr.inputRMS(targetj,fiberj,X);
+    end
+end
+
+%% Compile and run simulations
+
+root_cuda = fullfile(root,'hpc_code','lib');
+for i = 1:4
+    for j = 1:numel(file(i).fiber)
+        targetj = fullfile(target{i},sprintf('setup%03u',j));
+        cd(targetj);
+        copyfile(fullfile(root_cuda,'main.cu'),targetj);
+        system('nvcc -arch=sm_70 main.cu -o main_cuda');
+        system('./main_cuda');
+        system('rm -f main.cu main_cuda');
+    end
+end
+
